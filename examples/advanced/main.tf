5412b5f3c15770802b061c1ec798cf0e99c11f5e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-04-18 20:17:37 +0100

    chore(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.13.3 (#23)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -228,7 +228,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.13.2"
+  version           = "1.13.3"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
