e2793c03efd2ec1dfbf385e035bf5ef4f95b25e1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-03 19:39:51 +0000

    chore(deps): update terraform terraform-ibm-modules/logs-agent/ibm to v1.24.2

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -263,7 +263,7 @@ module "trusted_profile" {
 
 module "logs_agents" {
   source                        = "terraform-ibm-modules/logs-agent/ibm"
-  version                       = "1.24.1"
+  version                       = "1.24.2"
   cluster_id                    = module.iks_base.cluster_id
   cluster_resource_group_id     = module.resource_group.resource_group_id
   logs_agent_trusted_profile_id = module.trusted_profile.trusted_profile.id
