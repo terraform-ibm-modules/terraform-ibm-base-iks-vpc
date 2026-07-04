46dd19d1f27f225eb9d16ad5ce8a0dfccd4e8a85
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-07-04 12:23:55 +0100

    fix(deps): update dependencies (#42)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -228,7 +228,7 @@ locals {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.13.15"
+  version           = "1.13.16"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   plan              = "standard"
